c888a9adef6f1cd8ebf3a04fd428e82713d69154
Santiago Arocha <arochas846@gmail.com>
2022-03-20 23:57:45 -0300

    The last commit for now

--- fc.js
+++ fc.js
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="Santiago Arocha Pérez">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="assets/styles.css">
-    <title>Santi.dev</title>
-</head>
-<body>
-        <section>
-
-         <h1>Santiago Arocha,&nbsp;
-             <span class="output"></span>
-         </h1> 
-         
-         <script>
-             //text output
+//text output
 const output = document.querySelector(".output");
 //result
 let res;
@@ -106,34 +85,3 @@ function remove(){
     //Remove another letter
     charCount++;
 };
-
-         </script>
-
-        </section> 
-
-        <h2>Suscríbete</h2>
-        <form action="post">
-            <input type="text" name = "name" placeholder="Nombre Completo">
-            <input type="email" name="email" placeholder="Email">
-            <input type="submit" name="register">
-            <p>Selecciona un color</p>
-            <input type="color" name="color">
-        </form>
-        <!---
-        <?php 
-        /*
-            include("registrar.php");
-            */
-                ?>
-        
-        <p>Crack</p>
-        <p>Enfocado</p>
-        
-
-
-
-        <p>Vamo papa, vamo que se puede</p>
-        <a href="Practicals/index.php">Practicals</a>
-        --->
-</body>
-</html>
